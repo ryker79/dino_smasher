76d0eb249fac7777de92cb200292b0bfc614c4eb
Jeff Rice <jeff.rice@vml.com>
2018-08-21 07:47:29 -0500

    setup

--- dino_smasher.ino
+++ dino_smasher.ino
@@ -8,7 +8,8 @@
 
 // Define User Types below here or use a .h file
 //
-
+#include <Arduboy2.h>;
+Arduboy2 arduboy;
 
 // Define Function Prototypes that use User Types below here or use a .h file
 //
@@ -20,13 +21,29 @@
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
+	arduboy.begin();
+	arduboy.setFrameRate(45);
+	arduboy.display();
 
+	arduboy.initRandomSeed();
 
+	arduboy.clear();
 }
 
 // Add the main program code into the continuous loop() function
 void loop()
 {
+	if(!arduboy.nextFrame())
+	{
+		return;
+	}
 
+	arduboy.pollButtons();
 
+	arduboy.clear();
+
+	arduboy.setCursor(0, 0);
+	arduboy.print(F("Hello!"));
+
+	arduboy.display();
 }
