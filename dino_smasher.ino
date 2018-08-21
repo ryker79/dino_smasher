e50dd90c868637db72abc819623b500766f12a6e
Jeff Rice <jeff.rice@vml.com>
2018-08-21 08:40:12 -0500

    gamestate switch added

--- dino_smasher.ino
+++ dino_smasher.ino
@@ -11,12 +11,56 @@
 #include <Arduboy2.h>;
 Arduboy2 arduboy;
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
+#define GAME_TITLE 0
+#define GAME_PLAY 1
+#define GAME_OVER 2
+#define GAME_HIGH 3
+int game_state = GAME_TITLE;
 
 // Define Functions below here or use other .ino or cpp files
 //
+void title_screen()
+{
+	arduboy.setCursor(0, 0);
+	arduboy.print("Dino Smasher \n");
+}
+
+void game_play_screen()
+{
+	arduboy.setCursor(0, 0);
+	arduboy.print("Gameplay \n");
+}
+
+void game_over_screen()
+{
+	arduboy.setCursor(0, 0);
+	arduboy.print("Game Over \n");
+}
+
+void high_score_screen()
+{
+	arduboy.setCursor(0, 0);
+	arduboy.print("High Score: \n");
+}
+
+void game_loop()
+{
+	switch(game_state)
+	{
+	case GAME_TITLE:
+		title_screen();
+		break;
+	case GAME_PLAY:
+		game_play_screen();
+		break;
+	case GAME_OVER:
+		game_over_screen();
+		break;
+	case GAME_HIGH:
+		high_score_screen();
+		break;
+	}
+}
 
 // The setup() function runs once each time the micro-controller starts
 void setup()
@@ -41,9 +85,10 @@ void loop()
 	arduboy.pollButtons();
 
 	arduboy.clear();
-
-	arduboy.setCursor(0, 0);
-	arduboy.print(F("Hello!"));
+	
+	game_loop();
 
 	arduboy.display();
 }
+
+
